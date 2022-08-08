80d82dbb4b2a9bb30813ea41d8323348008254bb
Petr Sahula <pedro@luhas.cz>
2022-08-08 10:32:11 +0200

    Correct nubers of posts in a row based on page width

--- wp-content/themes/clean-wp-theme/archive.php
+++ wp-content/themes/clean-wp-theme/archive.php
@@ -4,34 +4,49 @@
     <div class="content-area group section">
         <!-- container -->
         <div class="container">
-
-        <?php
-        // Display optional category description
-        if (category_description()) { ?>
-            <div class="archive-meta"><h2><?php echo category_description(); ?></h2></div>
-        <?php } ?>
-
             <!-- row -->
             <div class="row">
+                <!-- col -->
+                <div class="col col-md-9 push-down-sm">
 
-                <?php
-                while (have_posts()) {
-                    the_post(); ?>
+                    <?php
+                    // Display optional category description
+                    if (category_description()) { ?>
+                        <div class="archive-meta"><h2><?php echo category_description(); ?></h2></div>
+                    <?php } ?>
 
-                    <!-- col -->
-                    <div class="col col-xs-4">
-                        <div class="frontpage">
-                            <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?>
-                            <p><?php the_title(); ?></p>
-                            <p><?php echo "<br><br>" . substr(wp_strip_all_tags(get_the_content()), 0, 210) . "...";?></p></a>
-                        </div>
+                    <!-- row -->
+                    <div class="row">
+                        <?php
+                        while (have_posts()) {
+                            the_post(); ?>
+
+                            <!-- col -->
+                            <div class="col col-xxs-12 col-xs-6 col-sm-4 col-md-6 col-lg-4 push-down-sm">
+                                <div class="frontpage-container">
+                                    <div class="frontpage">
+                                        <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?>
+                                        <p><?php the_title(); ?></p>
+                                        <p><?php echo substr(wp_strip_all_tags(get_the_content()), 0, 150) . "...";?></p></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /col -->
+                        <?php } ?>
                     </div>
-                    <!-- /col -->                
-                <?php } ?>
+                    <!-- /row -->
+
+                </div>
+                <!-- /col -->
+
+                <!-- col -->
+                <div class="col col-md-3 sidebar">
+                    <?php dynamic_sidebar( 'primary' ); ?>
+                </div>
+                <!-- /col -->
 
             </div>
             <!-- /row -->
-
         </div>
         <!-- /container -->
     </div>
--- wp-content/themes/clean-wp-theme/front-page.php
+++ wp-content/themes/clean-wp-theme/front-page.php
@@ -5,41 +5,42 @@
     <div class="container">
         <!-- row -->
         <div class="row">
-        <!-- col -->
-        <div class="col col-md-9 push-down-sm">
-            <h2>Vítejte na stránce eČasopisu Luhas.cz</h2>
+            <!-- col -->
+            <div class="col col-md-9 push-down-sm">
 
-            <h2>Nejnovější příspěvky</h2>
+                <h2>Nejnovější příspěvky</h2>
 
-            <!-- row -->
-            <div class="row">
+                <!-- row -->
+                <div class="row">
 
-                        <?php
-                        $the_query = new WP_Query([
-                            'post_type' => 'post',
-                            'posts_per_page' => 4
-                        ]);
-                        if ($the_query->have_posts()) { 
-                            while ($the_query->have_posts()) {
-                                $the_query->the_post(); ?>
-                                <!-- col -->
-                                <div class="col col-xs-6">
+                    <?php
+                    $the_query = new WP_Query([
+                        'post_type' => 'post',
+                        'posts_per_page' => 4
+                    ]);
+                    if ($the_query->have_posts()) { 
+                        while ($the_query->have_posts()) {
+                            $the_query->the_post(); ?>
+                            <!-- col -->
+                            <div class="col col-xs-6">
+                                <div class="frontpage-container">
                                     <div class="frontpage">
-                                            <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?>
-                                            <p><?php the_title(); ?></p>
-                                            <p><?php echo "<br><br>" . substr(wp_strip_all_tags(get_the_content()), 0, 210) . "...";?></p></a>
+                                        <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail(); ?>
+                                        <p><?php the_title(); ?></p>
+                                        <p><?php echo substr(wp_strip_all_tags(get_the_content()), 0, 210) . "...";?></p></a>
                                     </div>
                                 </div>
-                                <!-- /col -->
-                            <?php }
-                        } else {
-                            echo 'no pages found';
-                        }
-                        wp_reset_postdata(); ?>
+                            </div>
+                            <!-- /col -->
+                        <?php }
+                    } else {
+                        echo 'no pages found';
+                    }
+                    wp_reset_postdata(); ?>
+                </div>
+                <!-- /row -->
             </div>
-            <!-- /row -->
-        </div>
-        <!-- /col -->
+            <!-- /col -->
             <!-- col -->
             <div class="col col-md-3 sidebar">
                 <?php dynamic_sidebar( 'primary' ); ?>
